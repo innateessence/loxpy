085d8d2aecdc947dd68205b3dd3a5d0beb142934
JackofSpades707 <jackofspades707@hotmail.com>
2024-05-21 09:31:39 -0700

    update variable safety level thoughts

--- variable_state.py
+++ variable_state.py
@@ -2,13 +2,27 @@
 I want all variables to store some metadata in a very performant way that's easily portable to C
 """
 
+# https://ruby-doc.com/docs/ProgrammingRuby/html/taint.html#:~:text=Tainted%20Objects,shown%20in%20the%20code%20below.
+# Reference $SAFE levels in Ruby
+# Consider using a similar approach to Ruby's taint mechanism. But providing a more simplistic interface for the developer.
 
-class SafetyMetadata:
+from enum import Enum
+
+
+class BinaryEnum(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return 1 << count
+
+
+class VariableMetadata(BinaryEnum):
     UNKNOWN = 1  # This variable safety level is unknown (e.g. not initialized / not specified) This is the default
     TAINTED = 2  # This variable has been tainted (e.g. by user input)
     TRUSTED = 4  # This variable is trusted (e.g. by a security check)
 
-class VariableMetadata:
+
+class VariableMetaTraits:
+    """This class is a wrapper around the metadata of a variable. It allows us to easily interact with the metadata of a variable"""
+
     def __init__(self):
         self.metadata = 0
 
